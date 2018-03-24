8c14735bfecb31c91b08cc5fdb87768447d8aa90
paulaperoutka <paulaperoutka@gmail.com>
2018-03-24 01:01:31 -0400

    db complete with schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Carolina Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Quarterpounder");
+
+INSERT INTO burgers (burger_name) VALUES ("Shackburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+
+SELECT * FROM burgers;
